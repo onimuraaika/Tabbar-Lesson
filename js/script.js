637450933a11edb10f04d643a2ee863596a06047
onimuraaika <aika.onimura@gmail.com>
2020-10-11 13:25:31 +0900

    [Add]クリックイベントの設定、クリックしたときの動作を設定

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-$(function() {
-  $('.menu-trigger').on('click', function(event) {
-    $(this).toggleClass('active'); /*トグル処理、該当の(今回はactive)クラス属性がある時classが削除、ない時classを追加*/
-    $('#sp-menu').fadeToggle(); /*メニューを開いた時の状態を設定。.fadeToggleはフェードイン・フェードアウトを切り替えるメソッド*/
-    event.preventDefault();
-  });
+$('#tab-contents .tab[id != "tab1"]').hide(); /*id=tab-contents要素のうちtabクラスでid=1出ないものを非表示*/
+
+$('#tab-menu a').on('click', function(event) { /*id="tab-menu"のaタグがクリックされた時*/
+  $("#tab-contents .tab").hide(); /*id="tab-contents"内のいずれかのタブがクリックされたと時、そのタブ内の全コンテンツを非表示*/
+  $("#tab-menu .active").removeClass("active"); /*activeクラス(activeの時背景色が白・文字色が黒のcss)が削除対象*/
+  $(this).addClass("active"); /*thisは、タブがクリックされた時、activeクラスを追加*/
+  $($(this).attr("href")).show(); /*.attr()は、HTML要素の属性を取得したり設定できるメソッド。show()は、要素を表示するメソッド*/
+  event.preventDefault();
 });
